083a7286ef5b634943301196b00f90eda318548e
AtharvaParadkar <atharvaparadkar91@gmail.com>
2024-07-22 22:29:41 +0530

    Displaying Category Items

--- lib/data/dummy_data.dart
+++ lib/data/dummy_data.dart
@@ -6,32 +6,32 @@ const availableCategories = [
   Category(
     id: 'c1',
     title: 'Italian',
-    color: Colors.purple,
+    color: Color.fromARGB(255, 205, 34, 235),
   ),
   Category(
     id: 'c2',
     title: 'Quick & Easy',
-    color: Colors.red,
+    color: Color.fromARGB(255, 248, 0, 0),
   ),
   Category(
     id: 'c3',
     title: 'Hamburgers',
-    color: Colors.orange,
+    color: Color.fromARGB(255, 252, 189, 0),
   ),
   Category(
     id: 'c4',
     title: 'German',
-    color: Colors.amber,
+    color: Colors.orange,
   ),
   Category(
     id: 'c5',
     title: 'Light & Lovely',
-    color: Colors.blue,
+    color: Color.fromARGB(255, 0, 0, 255),
   ),
   Category(
     id: 'c6',
     title: 'Exotic',
-    color: Colors.green,
+    color: Color.fromARGB(255, 0, 255, 0),
   ),
   Category(
     id: 'c7',
@@ -41,16 +41,16 @@ const availableCategories = [
   Category(
     id: 'c8',
     title: 'Asian',
-    color: Colors.lightGreen,
+    color: Color.fromARGB(255, 147, 245, 34),
   ),
   Category(
     id: 'c9',
     title: 'French',
-    color: Colors.pink,
+    color: Color.fromARGB(255, 243, 47, 112),
   ),
   Category(
     id: 'c10',
     title: 'Summer',
-    color: Colors.teal,
+    color: Color.fromARGB(255, 6, 216, 132),
   ),
 ];
