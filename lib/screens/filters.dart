55f735e6ac9612814ddafcb1231ca85c007fb2c5
AtharvaParadkar <atharvaparadkar91@gmail.com>
2024-10-23 21:16:34 +0530

    Applying Filters

--- lib/screens/filters.dart
+++ lib/screens/filters.dart
@@ -10,7 +10,9 @@ enum Filter {
 }
 
 class FiltersScreen extends StatefulWidget {
-  const FiltersScreen({super.key});
+  const FiltersScreen({super.key, required this.currentFilter});
+
+  final Map<Filter,bool> currentFilter;
 
   @override
   State<FiltersScreen> createState() => _FiltersScreenState();
@@ -21,6 +23,16 @@ class _FiltersScreenState extends State<FiltersScreen> {
   var _lactoseFreeFilterSet = false;
   var _vegetarianFilterSet = false;
   var _veganFilterSet = false;
+
+@override
+  void initState() {
+    super.initState();
+    _glutenFreeFilterSet=widget.currentFilter[Filter.glutenFree]!;
+    _lactoseFreeFilterSet=widget.currentFilter[Filter.lactoseFree]!;
+    _vegetarianFilterSet=widget.currentFilter[Filter.vegetarian]!;
+    _veganFilterSet=widget.currentFilter[Filter.vegan]!;
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
